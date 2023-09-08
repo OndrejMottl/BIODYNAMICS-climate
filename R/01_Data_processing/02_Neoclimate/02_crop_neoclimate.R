502d3d4ae992c1ed530bd52de60f7a2ff5153404
OndrejMottl <ondrej.mottl@gmail.com>
2023-09-08 11:37:07 +0200

    02_crop_neoclimate: crop out only the land

---
+++ R/01_Data_processing/02_Neoclimate/02_crop_neoclimate.R
@@ -0,0 +1,65 @@
+#----------------------------------------------------------#
+#
+#
+#                     Abiotic data
+#
+#                crop climate data CHELSA
+#
+#
+#                       O. Mottl
+#                         2023
+#
+#----------------------------------------------------------#
+
+#----------------------------------------------------------#
+# 0. Setup -----
+#----------------------------------------------------------#
+
+library(here)
+
+# Load configuration
+source(
+  here::here(
+    "R/00_Config_file.R"
+  )
+)
+
+#----------------------------------------------------------#
+# 2. Load country shapefile -----
+#----------------------------------------------------------#
+
+shapefile_land <-
+  sf::st_read(
+    here::here(
+      "Data/Input/Countries/ne_110m_admin_0_countries.shp"
+    )
+  )
+
+file_names <-
+  list.files(
+    here::here(
+      "Data/Processed/Neoclimate"
+    ),
+    full.names = TRUE,
+    recursive = TRUE,
+    pattern = "tif"
+  )
+
+
+#----------------------------------------------------------#
+# 3. Downscale data -----
+#----------------------------------------------------------#
+
+future::plan("multisession", workers = parallelly::availableCores())
+
+furrr::future_walk(
+  .progress = TRUE,
+  .x = file_names,
+  .f = ~ downscale_and_crop_tif_data(
+    file_path = .x,
+    dir = here::here("Data/Processed/Neoclimate/Cropped"),
+    sel_factor = 1,
+    only_land = TRUE,
+    shapefile_land = shapefile_land
+  )
+)
